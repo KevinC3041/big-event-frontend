c2e59073fce1050b313924a221099a49beb47a0a
KevinC <xchen935@wisc.edu>
2024-06-17 17:03:03 +0800

    useaxios request interceptor to implement pinia

--- src/api/article.js
+++ src/api/article.js
@@ -6,5 +6,6 @@ import { useTokenStore } from '@/stores/token.js'
 export const articleCategoryListService = () => {
     const tokenStore= useTokenStore();
     // 在pinia中定义的响应式数据,都不需要.value
-    return request.get('/category', {headers:{'Authorization': tokenStore.token}});
+    // return request.get('/category', {headers:{'Authorization': tokenStore.token}});
+    return request.get('/category');
 }
